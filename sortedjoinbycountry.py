deda31e679838996eefb35d34a3d45c77ca16164
Jordan Bramble <jordanbramble@gmail.com>
2016-02-26 07:00:52 +0000

    problem 13

--- sortedjoinbycountry.py
+++ sortedjoinbycountry.py
@@ -12,6 +12,7 @@ from weblog import Weblog       # imports class defined in weblog.py
 from mrjob.step import MRStep
 
 class FwikiMaxmindJoin(MRJob):
+    SORT_VALUES = True
     def mapper(self, _, line):
         # Is this a weblog file, or a MaxMind GeoLite2 file?
         filename = mrjob.compat.jobconf_from_env("map.input.file")
---
+++ wikipedia_stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+import mrjob
+import mrjob.compat
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import sys
+
+class wikipedia_stats(MRJob):
+    SORT_VALUES = True
+
+    def mapper(self, _, line):
+    article = line.split('\t')[2]
+        date = article.split(' ')
+    day = date[0].split('-')
+        specificmonth = day[0]+'-'+day[1]
+        yield specificmonth, 1
+
+    def reducer(self, key, value):
+    yield key, sum(value)
+
+    def mapper_two(self, key, value):
+        yield "Wiki", (key, value)
+
+    def reducer_two(self, key, value):
+        for v in value:    
+            yield v[0], v[1]
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(mapper=self.mapper_two,
+                   reducer=self.reducer_two) ]
+
+if __name__ == "__main__":
+    wikipedia_stats.run()
